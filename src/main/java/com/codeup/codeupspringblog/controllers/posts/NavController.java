87fa6b4fd424ed57eebba3e899a071d56613754e
aharownjackson <aharownmjackson@gmail.com>
2023-03-28 12:21:25 -0500

    added show a single post feature and crud functionality

--- src/main/java/com/codeup/codeupspringblog/controllers/posts/NavController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/posts/NavController.java
@@ -1,6 +1,7 @@
 package com.codeup.codeupspringblog.controllers.posts;
 
 import com.codeup.codeupspringblog.models.posts.Post;
+import com.codeup.codeupspringblog.repositories.PostRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,13 +12,20 @@ import java.util.List;
 
 @Controller
 public class NavController {
+
+    // DEPENDENCY INJECTION //
+
+    private PostRepository postsDao;
+
+    public NavController(PostRepository postsDao) {
+        this.postsDao = postsDao;
+    }
+
+    // END DEPENDENCY INJECTION //
+
     @GetMapping("/posts")
     public String postsIndex (Model model) {
-        List<Post> posts = new ArrayList<>(Arrays.asList(
-                new Post("Index Post Test 1", "This is the first of three index post tests. It should be displayed with three other posts"),
-                new Post ("Index Post Test 2", "This is the second of three index post tests. It should be displayed with three other posts."),
-                new Post ("Index Post Test 3", "This is the third of three index post tests. It should be displayed with three other posts.")
-        ));
+        List<Post> posts = postsDao.findAll();
         model.addAttribute("posts", posts);
         return "posts/index";
     }
--- src/main/java/com/codeup/codeupspringblog/controllers/posts/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/posts/PostController.java
@@ -1,6 +1,7 @@
 package com.codeup.codeupspringblog.controllers.posts;
 
 import com.codeup.codeupspringblog.models.posts.Post;
+import com.codeup.codeupspringblog.repositories.PostRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,20 +9,37 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Optional;
+
 @Controller
 public class PostController {
 
+    // DEPENDENCY INJECTION //
+
+    private PostRepository postsDao;
+
+    public PostController(PostRepository postsDao) {
+        this.postsDao = postsDao;
+    }
+
+    // END DEPENDENCY INJECTION //
+
     @GetMapping("/posts/{id}")
-    public String postById (@PathVariable long id, Model model) {
-        Post post = new Post("Single Test Post", "This is a test post, it is supposed to show an individual post.");
-        model.addAttribute("post", post);
+    public String postById(@PathVariable long id, Model model) {
+        Optional<Post> post = postsDao.findById(id);
+        if (post.isPresent()) {
+            model.addAttribute("post", post.get());
+
+        }
         return "posts/show";
     }
 
-    @PostMapping("/create")
+    @PostMapping("/create/post")
     @ResponseBody
-    public String createPost () {
-        return "create a new post";
+    public String createPost() {
+        Post post = new Post("this is a created post", "This is a post that was created using the create");
+        postsDao.save(post);
+        return "redirect:/posts";
     }
 
 }
